c11a50d15a9a16d4d087f953e350f04fdf433677
antonin <antonin.chafiol@gmail.com>
2025-12-10 12:22:07 +0000

    feat(ano): Changed to compressed u32 + Working deid

--- commands/ano/src/lib.rs
+++ commands/ano/src/lib.rs
@@ -5,7 +5,7 @@ use dashmap::DashMap;
 pub mod models;
 pub mod iod;
 
-use crate::models::{DEID_HASH, DEID_MAP};
+use crate::models::{DEID_MAP};
 
 pub fn threading_handling(
     files: Vec<PathBuf>, 
@@ -50,7 +50,18 @@ pub fn single_thread_process(
     with_pixel_data: bool, 
     uid_map: &Arc<DashMap<String, String>>
 ) -> Result<()> {
-    let data = de_identify_file(input_path.clone(), with_pixel_data, verbose, uid_map)?; 
+    let mut data = if !with_pixel_data {
+        OpenFileOptions::new()
+            .read_until(dicom_dictionary_std::tags::PIXEL_DATA)
+            .open_file(input_path.clone())?
+    } else {
+        open_file(input_path.clone())?
+    };
+    
+    // TODO: Check result
+    let process_result = de_identify(&mut data, uid_map);
+
+    // let data = de_identify_file(input_path.clone(), with_pixel_data, verbose, uid_map)?; 
 
     if *dry {
         if verbose {
@@ -81,83 +92,19 @@ pub fn single_thread_process(
     Ok(())
 }
 
-fn de_identify_file (
-    file_path: PathBuf, 
-    // profile: Profile :TODO: Later implement the profile to match the right 
-    // one in policyAction
-    with_pixel_data: bool,
-    verbose: bool,
-    uid_map: &Arc<DashMap<String, String>>
-) -> Result<FileDicomObject<InMemDicomObject>> {
-    
-    let mut data = if !with_pixel_data {
-        OpenFileOptions::new()
-            .read_until(dicom_dictionary_std::tags::PIXEL_DATA)
-            .open_file(file_path)?
-    } else {
-        open_file(file_path)?
-    };
-    // let metadata = data.meta();
-    // let mut data = data.into_inner();
-
-    for t in data.tags() {
-        if let Some(el) = data.get(t){
-            if el.header().vr == VR::SQ {
-                // Rebuild and recurse
-            }
-            if let Some(oui) = DEID_MAP.get((t.0, t.1)){
-                oui.apply(data, tag, vr, uid_map);
-            }
-        }
-
-    }
-    
-
-
-
-
-    for (key, value) in DEID_HASH.entries() {
-        let rec_tag: Tag = Tag{0: key.0, 1:key.1};
-        // Check if in 
-        if let Ok(elem) = data.element(rec_tag) {
-            let vr = elem.vr();
-            value.basic.process(&mut data, &rec_tag, &vr, uid_map)?;
-        } else {
-            continue;
-        }
-    }
-    Ok(data)
-
-
-    // for element in data.into_iter() {
-    //     let tag = element.header().tag;
-    //     // TODO: Get both u16 and change the DEID HASH from Tag to (u16, u16)
-    //     let vr = element.header().vr;
-    //     // Check if contains
-    //     // if yes: apply the 1st profile
-    //     // + check for the rest of profiles to add afterwards 
-    //     // else : pass
-    // }
-    // }
-    // Ok(data)
-
-
-
-}
-
-
 fn de_identify(
     data: &mut InMemDicomObject, 
     uid_map: &Arc<DashMap<String, String>>,
-) {
+) -> Result<()> {
     let tags: Vec<Tag> = data.tags().collect();
     for tag in tags {
-        let vr = data.get(tag).unwrap().vr();
         // Get all tags and retrieve the VR from it 
+        let vr = data.get(tag).unwrap().vr();
             match vr {
+                // If a Sequence, Rebuild a InMemDicomObject from it and recurse 
+                // (Allow to search for inner tags that could match)
                 VR::SQ => {
                     let _ = data.update_value_at(tag, |val| {
-                    // If a Sequence, Rebuild a InMemDicomObject from it and recurse
                     if let Some(items) = val.items_mut() {
                         for item in items {
                             de_identify(item, uid_map);
@@ -165,13 +112,16 @@ fn de_identify(
                     }
                     });
                 },
+                // If not a sequence, check if we have the attribute/tag within our policy hashmap
                 _ => {
-                    // If not a sequence, check if we have the attribute/tag within our policy hashmap
-                    let tag_tuple = (tag.0, tag.1);
-                    if let Some(policy_action) = DEID_MAP.get(&tag_tuple){
-                        policy_action.apply(data, &tag_tuple, uid_map);
+                    // Compress the tag (two u16) into a u32 for the hashmap checks
+                    let compressed_tag: u32 = ((tag.0 as u32) << 16) + tag.1 as u32;
+                    
+                    if let Some(policy_action) = DEID_MAP.get(&compressed_tag){
+                        policy_action.apply(data, &compressed_tag, &vr, uid_map);
                     }
                 },
             }
     }
+    Ok(())
 }
--- commands/ano/src/models.rs
+++ commands/ano/src/models.rs
@@ -13,44 +13,90 @@ pub enum Action {
     ReplaceDummy,
     ReplaceUID,
     Keep,
-    Zero,
 }
 
-pub static DEID_MAP: phf::Map<(u16, u16), Action> = phf_map!{
-    (0x0010, 0x0010) => Action::Remove,       // PatientName
-    (0x0010, 0x0020) => Action::Remove,       // PatientID
-    (0x0010, 0x0030) => Action::Empty,        // PatientBirthDate
-    (0x0008, 0x0020) => Action::Empty,        // StudyDate
-    (0x0008, 0x0030) => Action::Empty,        // StudyTime
-    (0x0020, 0x000D) => Action::ReplaceUID,   // StudyInstanceUID
-    (0x0020, 0x000E) => Action::ReplaceUID,   // SeriesInstanceUID
-    (0x0008, 0x0018) => Action::ReplaceUID,   // SOPInstanceUID
-    (0x0008, 0x0080) => Action::Empty,        // InstitutionName
-    (0x0008, 0x0050) => Action::Empty,        // AccessionNumber
-    (0x0008, 0x0090) => Action::Remove,       // Referring Physician
-    (0x0008, 0x1030) => Action::Empty,        // StudyDescription
-    (0x0018, 0x1000) => Action::Empty,        // DeviceSerialNumber
+pub static DEID_MAP: phf::Map<u32, Action> = phf_map!{
+    0x00080018 => Action::ReplaceUID,   // SOPInstanceUID
+    0x00080020 => Action::Empty,        // StudyDate
+    0x00080030 => Action::Empty,        // StudyTime
+    0x00080050 => Action::Empty,        // AccessionNumber
+    0x00080080 => Action::Empty,        // InstitutionName
+    0x00080090 => Action::Remove,       // Referring Physician
+    0x00081030 => Action::Empty,        // StudyDescription
+    0x00100010 => Action::Remove,       // PatientName
+    0x00100020 => Action::Remove,       // PatientID
+    0x00100030 => Action::Empty,        // PatientBirthDate
+    0x00181000 => Action::Empty,        // DeviceSerialNumber
+    0x0020000D => Action::ReplaceUID,   // StudyInstanceUID
+    0x0020000E => Action::ReplaceUID,   // SeriesInstanceUID
 };
 
 impl Action {
-    pub fn apply(&self, data: &mut InMemDicomObject, tag: &(u16, u16), uid_map: &Arc<DashMap<String, String>>) {
+    pub fn apply(&self, 
+        data: &mut InMemDicomObject, 
+        u32_tag: &u32, 
+        vr: &VR,
+        uid_map: &Arc<DashMap<String, String>>
+    ) -> Result<()> {
+        // Rebuilding a coherent taf from the u33 compressed version
+        let built_tag = Tag {0: (u32_tag >> 16) as u16,
+                             1: (u32_tag & 0xFFFF) as u16
+        };
         match self {
             Action::Empty        => {
-                
+                data.update_value_at(built_tag, |v|{
+                    *v.primitive_mut().unwrap() = PrimitiveValue::Empty;
+                })?;
+                Ok(())
             },
-            Action::Remove       => {},
-            Action::ReplaceDummy => {},
-            Action::ReplaceUID   => {},
-            Action::Keep         => {},
-            Action::Zero         => {},
+            Action::Remove       => {
+                let _ = data.remove_element(built_tag);
+                Ok(())
+            },
+            Action::ReplaceDummy => {
+                data.update_value_at(built_tag, |v|{
+                    *v.primitive_mut().unwrap() = dummy_from_vr(vr);
+                })?;
+                Ok(())
+            },
+            Action::ReplaceUID   => {
+                if let Ok(old_val) = data.value_at(built_tag){
+                    if let Some(old_val) = old_val.primitive(){
+                        // Get the final String 
+                        let old_val = old_val.to_string();
 
+                        // Check if it's contained within the map
+                        if let Some(uid_match) = uid_map.get(&old_val) {
+                            // Found the right entry / replacing the current UID with the generated
+                            // one from the uid map
+                            data.update_value_at(built_tag, |v|{
+                                // I'm not using an "if let" statement here as 
+                                *v.primitive_mut().unwrap() = PrimitiveValue::from(uid_match.value().as_str());
+                            })?;
+                        } else {
+                            // Generate a new UUID 
+                            let new_generated_uid = generate_uid();
+                            // Change the current field to the new one
+                            data.update_value_at(built_tag, |v|{
+                                *v.primitive_mut().unwrap() = PrimitiveValue::from(new_generated_uid.clone());
+                            })?;
+                            // Add the change to the hashmap
+                            uid_map.insert(old_val, new_generated_uid);
+                        }
+                    }
+                }
+                Ok(())
+            },
+            Action::Keep         => {
+                Ok(())
+            },
         }
-    }
-    pub fn apply_inmemobject(&self, data: &mut InMemDicomObject, tag: &(u16, u16) ,uid_map: &mut DashMap<String, String>) {
-        
     }
 }
 
+fn generate_uid() -> String {
+    format!("2.25.{}", Uuid::new_v4().to_u128_le())
+}
 
 // --------- Clinical Branch ---------
 
